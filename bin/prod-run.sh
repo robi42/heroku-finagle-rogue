98c7f04ad77194e7accf025946658a9375f9e814
Robert Thurnher <r.thurnher@gmail.com>
2011-09-28 22:24:35 +0200

    Refine runner scripts.

--- bin/prod-run.sh
+++ bin/prod-run.sh
@@ -3,5 +3,5 @@
 export JAVA_OPTS="-Xmx1536M -Xss512k -XX:+UseCompressedOops"
 export LIFT_PROD="-Drun.mode=production"
 
-exec sh `dirname $0`/run.sh >> `dirname $0`/../logs/stdout.log 2>> `dirname $0`/../logs/errout.log &
+nohup sh `dirname $0`/run.sh >> `dirname $0`/../logs/stdout.log 2>> `dirname $0`/../logs/errout.log &
 echo $! > PROD_PID
--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nohup java $JAVA_OPTS \
+exec java $JAVA_OPTS \
 -cp `dirname $0`/../build/application.jar:`dirname $0`/../build/scala-library.jar:`dirname $0`/../build/dependencies.jar \
 -Dlogback.configurationFile=`dirname $0`/../config/logback.xml \
 $LIFT_PROD \
