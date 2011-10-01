aba57cb91c0376e9ea152a40bfad02decdcac920
Robert Thurnher <r.thurnher@gmail.com>
2011-10-01 15:53:41 +0200

    Formatting.

--- src/main/scala/cache.scala
+++ src/main/scala/cache.scala
@@ -1,11 +1,12 @@
 package com.robert42.ft
 
-import net.spy.memcached.MemcachedClient
 import net.liftweb.util.Props
+import net.spy.memcached.MemcachedClient
 import java.net.InetSocketAddress
 
 object Memcached {
   private lazy val host = Props.get("memcached.host", "localhost")
   private lazy val port = Props.getInt("memcached.port", 11211)
+
   lazy val cache = new MemcachedClient(new InetSocketAddress(host, port))
 }
